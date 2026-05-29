7e3ab6287c4ccbc943a9ed57ff58ed61b11607e6
Ashguard85 <philipp-haas@bluewin.ch>
2026-05-29 15:49:39 +0200

    Add files via upload

--- chez-philipp/docker/app-entrypoint.sh
+++ chez-philipp/docker/app-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
-echo ">>> Injecting config..."
-sed -i "s|CALDAV_URL_PLACEHOLDER|http://${CALDAV_HOST}:${CALDAV_PORT}/${CALDAV_USER}/calendar/|g" /usr/share/nginx/html/index.html
-sed -i "s|CALDAV_USER_PLACEHOLDER|${CALDAV_USER}|g"  /usr/share/nginx/html/index.html
-sed -i "s|CALDAV_PASS_PLACEHOLDER|${CALDAV_PASS}|g"  /usr/share/nginx/html/index.html
-sed -i "s|ADMIN_PIN_VALUE|${ADMIN_PIN}|g"             /usr/share/nginx/html/index.html
-echo ">>> Starting nginx..."
+echo ">>> Injecting config into index.html..."
+
+CALDAV_FULL="http://${CALDAV_HOST}:${CALDAV_PORT}/${CALDAV_USER}/calendar/"
+
+sed -i "s|%%CALDAV_URL%%|${CALDAV_FULL}|g"  /usr/share/nginx/html/index.html
+sed -i "s|%%CALDAV_USER%%|${CALDAV_USER}|g" /usr/share/nginx/html/index.html
+sed -i "s|%%CALDAV_PASS%%|${CALDAV_PASS}|g" /usr/share/nginx/html/index.html
+sed -i "s|%%ADMIN_PIN%%|${ADMIN_PIN}|g"     /usr/share/nginx/html/index.html
+
+echo ">>> Config injected. Starting nginx..."
 exec nginx -g 'daemon off;'
